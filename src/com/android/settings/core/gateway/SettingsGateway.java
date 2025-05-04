xiaomi-sm8450-mondrian/android_packages_apps_Settings

Settings: Allow to quickly open Running Services from meminfo view neobuddy89: Updated for A14.
Change-Id: I252ce85bda3daab5c5e4db097a6fd743365bc309
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -46,6 +46,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appcompat.UserAspectRatioDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
@@ -404,7 +405,8 @@ public class SettingsGateway {
             LockScreen.class.getName(),
             Themes.class.getName(),
             Toolbox.class.getName(),
-            SmartPixels.class.getName()
+            SmartPixels.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -455,6 +457,7 @@ public class SettingsGateway {
             Settings.PersonalizationsLockscreenActivity.class.getName(),
             Settings.PersonalizationsToolboxActivity.class.getName(),
             Settings.PersonalizationsThemesActivity.class.getName(),
-            Settings.SmartPixelsActivity.class.getName()
+            Settings.SmartPixelsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }

YAAP-Extended/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -424,7 +424,8 @@ public class SettingsGateway {
             VolumeSteps.class.getName(),
             PrivateDnsSettings.class.getName(),
             RunningServices.class.getName(),
-            PersonalizationsFragment.class.getName()
+            PersonalizationsFragment.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -478,6 +479,7 @@ public class SettingsGateway {
             Settings.WirelessDebuggingActivity.class.getName(),
             Settings.PrivateDnsSettingsActivity.class.getName(),
             Settings.RunningServicesDevActivity.class.getName(),
-            Settings.PersonalizationsActivity.class.getName()
+            Settings.PersonalizationsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }